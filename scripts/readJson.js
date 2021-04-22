cebd519c5d35a1097f4af48bb6df48805ca65611
Vaibhav Pratihar <vaibhavpratihar1771@gmail.com>
2021-04-22 13:07:51 +0530

    added functionality to open in new tab

--- scripts/readJson.js
+++ scripts/readJson.js
@@ -40,7 +40,8 @@ function verifiedCheck() {
 
 function redirect() {
     $(".city-name").click(function(event) {
-        window.location = 'https://twitter.com/search?q=' + verifiedCheck() + '+%28+' + $(this).text() + '+%28' + params + '%29+-%22not+verified%22+-%22unverified%22+-%22needed%22+-%22required%22&f=live';
+        var url = 'https://twitter.com/search?q=' + verifiedCheck() + '+%28+' + $(this).text() + '+%28' + params + '%29+-%22not+verified%22+-%22unverified%22+-%22needed%22+-%22required%22&f=live';
+        window.open(url, "_blank");
     });
 }
 
